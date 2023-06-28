60de64e721f5db55be0d608c395c0053b7eb935c
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-06-28 16:36:45 -0300

    req 8 - Done - add search_by_word func

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,6 +21,23 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    results = []
+    report = {}
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        occurence = []
+
+        for index, content in enumerate(file["linhas_do_arquivo"]):
+            if word in content.lower():
+                occurence.append({"linha": index + 1, "conteudo": content})
+        report["palavra"] = word
+        report["arquivo"] = file["nome_do_arquivo"]
+        report["ocorrencias"] = occurence
+
+        if occurence != []:
+            results.append(report)
+    return results
 
 
 # iniciando o projeto
